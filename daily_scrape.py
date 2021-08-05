3b18de96704ad12ca91448d9f5c46cff795d69cf
gsajko <grzegorz.sajko@protonmail.com>
2021-08-05 11:05:31 +0200

    added monthly scrape

---
+++ daily_scrape.py
@@ -0,0 +1,57 @@
+# %%
+import requests
+
+import os
+from datetime import datetime
+import pandas as pd
+
+# %%
+# import json
+# auth_path: str = "config/auth.json"
+# auth = json.load(open(auth_path))
+# TOKEN = auth["token"]
+TOKEN = os.environ["TOKEN"]
+fileList = os.listdir("data/week/")
+week_of_year = datetime.now().strftime("%y_%V")
+
+# %%
+if f"{week_of_year}.csv" in fileList:
+    df = pd.read_csv(f"data/week/{week_of_year}.csv")
+else:
+    columns = [
+        "SiteId",
+        "FuelId",
+        "CollectionMethod",
+        "TransactionDateUtc",
+        "Price",
+    ]
+    df = pd.DataFrame(columns=columns)
+
+# %%
+url = "https://fppdirectapi-prod.fuelpricesqld.com.au/Price/GetSitesPrices?countryId=21&geoRegionLevel=3&geoRegionId=1"
+
+payload = {}
+headers = {
+    "Authorization": f"FPDAPI SubscriberToken={TOKEN}",
+    "Content-Type": "application/json",
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+if response.status_code == 200:
+    print(response)
+else:
+    print(response)
+    print(response.reason)
+# %%
+df_scraped = pd.DataFrame(response.json()["SitePrices"])
+# check what month it is
+# %%
+
+# %%
+df = df.append(df_scraped, sort=False)
+# %%
+df.drop_duplicates(inplace=True)
+# %%
+df.to_csv(f"data/week/{week_of_year}.csv", index=False)
+
+# %%
---
+++ montly_scrape.py
@@ -0,0 +1,43 @@
+# %%
+import requests
+import json
+import os
+import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
+# %%
+# url
+url = "https://www.data.qld.gov.au/dataset/fuel-price-reporting"
+html = urllib.request.urlopen(url)
+soup = BeautifulSoup(html, "html.parser")
+# %% getting data from script into df
+data = json.loads(soup.find("script", type="application/ld+json").string)
+list_id = [i for i in data["@graph"]]
+df = pd.DataFrame(list_id)
+df = df[df["schema:encodingFormat"] == "CSV"]
+
+# %% clean up df
+columns_list = df.columns
+columns_list = [col.replace("schema:", "") for col in columns_list]
+df.columns = columns_list
+df = df[["@id", "contentSize", "description", "encodingFormat", "name", "url"]]
+df["@id"] = df["@id"].str.replace(
+    "https://www.data.qld.gov.au/dataset/cb7a63ac-d6e1-4e78-9f9b-a23969ca052c/resource/",
+    "",
+)
+
+# %% scrape if needed
+fileList = os.listdir("data/month")
+
+for csv_url in df["url"]:
+    name = csv_url.split("/")[-1]
+    if name in fileList:
+        pass
+    else:
+        print(f"downloading {name}")
+        req = requests.get(csv_url)
+        url_content = req.content
+        csv_file = open(f"data/month/{name}", "wb")
+        csv_file.write(url_content)
+        csv_file.close()
+# %%
